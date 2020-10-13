9b1a513b0d241123c3fb82125c785d937f7466a6
Simon Castagna <sd.castagna@gmail.com>
2020-10-13 19:44:40 +0200

    added Slide model for homepage

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Slide(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(upload_to='slides', null=True, blank=True)
+    cta_txt = models.CharField(max_length=30)
+    cta_link = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
