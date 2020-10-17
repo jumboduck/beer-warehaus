d9fa6d7c451272827852d7e750bcd82f6587fe7a
Simon Castagna <sd.castagna@gmail.com>
2020-10-17 14:14:46 +0200

    updated css on large screen sizes

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from .models import Product, Producer, Style
+from .models import Product, Producer, Style, Category
 
-# Register your models here.
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'producer',
+        'name',
+        'volume',
+    )
+
+    ordering = ('sku',)
+
+
+admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Producer)
 admin.site.register(Style)
--- products/models.py
+++ products/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 
 
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=260)
+    friendly_name = models.CharField(max_length=260, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
 class Style(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, default='Other')
 
     def __str__(self):
         return self.name
@@ -30,7 +45,7 @@ class Product(models.Model):
         ('can', 'can'),
     ]
 
-    style = models.ForeignKey('Style', null=True, blank=True, on_delete=models.SET_NULL)
+    style = models.ForeignKey('Style', on_delete=models.CASCADE, default='Other')
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     producer = models.ForeignKey('Producer', null=True, blank=True, on_delete=models.SET_NULL)
