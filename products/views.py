a74495e435873e4e4440e50385693a44ead30992
Simon Castagna <sd.castagna@gmail.com>
2020-10-17 19:29:55 +0200

    added view to add a new producer to db

--- products/views.py
+++ products/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 from .forms import ProducerForm
 
 
 def add_producer(request):
-    form = ProducerForm
+    if request.method == 'POST':
+        form = ProducerForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse(add_producer))
+
+    else:
+        form = ProducerForm
+
     template = 'products/add_producer.html'
     context = {
         'form': form,
