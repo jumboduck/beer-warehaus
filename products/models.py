edd86d4e539a2ddd0c4c1730916e0bc6ea6e5684
Simon Castagna <sd.castagna@gmail.com>
2020-10-17 17:07:25 +0200

    update to Producer model to include boolean highlight field and a default folder for images

--- products/models.py
+++ products/models.py
@@ -32,7 +32,8 @@ class Producer(models.Model):
     location = models.CharField(max_length=254)
     description = models.TextField()
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(upload_to='producers', null=True, blank=True)
+    highlight = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
