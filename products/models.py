bc798805b98465eead39f0de152836a61e60a2b9
Simon Castagna <sd.castagna@gmail.com>
2020-10-18 12:53:18 +0200

    new product can be added form untappd and its image url fills the image field

--- products/models.py
+++ products/models.py
@@ -80,7 +80,23 @@ class Product(models.Model):
     new_product = models.BooleanField(default=False)
     in_stock = models.BooleanField(default=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(upload_to='beers', null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    """
+    Update to save method so that any image in the 'Image url' field
+    will be saved on the server as the image field.
+    """
+    def save(self, *args, **kwargs):
+        if self.image_url and not self.image:
+            image_url = self.image_url
+            file_name = image_url.rsplit('/', 1)[-1]
+            response = requests.get(image_url)
+
+            img_temp = NamedTemporaryFile(delete=True)
+            img_temp.write(response.content)
+            img_temp.flush()
+            self.image.save(f'{self.name}_{file_name}', File(img_temp))
+        super(Product, self).save(*args, **kwargs)
