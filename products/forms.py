25fa4d3b3038742557a99dc93366781a5b7410ca
Simon Castagna <sd.castagna@gmail.com>
2020-10-18 19:26:07 +0200

    updated styles to add_producer page

--- products/forms.py
+++ products/forms.py
@@ -8,6 +8,12 @@ class ProducerForm(forms.ModelForm):
         model = Producer
         fields = '__all__'
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'dark-border'
+
 
 class ProductForm(forms.ModelForm):
 
