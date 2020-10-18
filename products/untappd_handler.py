787fe54c6e9da30851e64d57c2aa2c323342f64b
Simon Castagna <sd.castagna@gmail.com>
2020-10-18 12:07:31 +0200

    added search beer method to untappd handler class

--- products/untappd_handler.py
+++ products/untappd_handler.py
@@ -26,3 +26,14 @@ class UntappdHandler:
         producer_info = response.json()['response']['brewery']
 
         return producer_info
+
+    def search_beer(query):
+        # Build url from request https://api.untappd.com/v4/search/beer?q=Pliny
+        url = f'https://api.untappd.com/v4/search/beer?q={query}&client_id={settings.UNTAPPD_CLIENT_ID}&client_secret={settings.UNTAPPD_SECRET}'
+        response = requests.get(url)
+
+        # Keep only items in the response
+        beer_list = response.json()['response']['beers']['items']
+
+        # Return the 10 first results
+        return beer_list[:10]
