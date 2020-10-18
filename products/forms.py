21bdce3da3948c573880dca41d997ddaaaf70424
Simon Castagna <sd.castagna@gmail.com>
2020-10-18 19:52:53 +0200

    added styling to pages to add producers and products

--- products/forms.py
+++ products/forms.py
@@ -20,3 +20,9 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'dark-border'
