84eb5db33a2862340f1f5ebbf93f1e13101717e8
Simon Castagna <sd.castagna@gmail.com>
2020-10-24 22:21:05 +0200

    added cart context processor

--- beer_warehaus/settings.py
+++ beer_warehaus/settings.py
@@ -89,6 +89,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.media',
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.cart_contents',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
---
+++ cart/contexts.py
@@ -0,0 +1,13 @@
+def cart_contents(request):
+
+    cart_items = []
+    total = 0
+    product_count = 0
+
+    context = {
+        'cart_items': cart_items,
+        'total': total,
+        'product_count': product_count,
+    }
+
+    return context
