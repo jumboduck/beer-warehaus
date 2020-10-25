b84c0badd61b4245ba0bd4a7d15820b8ae0ee209
Simon Castagna <sd.castagna@gmail.com>
2020-10-25 09:36:08 +0100

    update to context processor

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,12 +1,27 @@
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
+
 def cart_contents(request):
 
     cart_items = []
-    total = 0
+    grand_total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        grand_total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     context = {
         'cart_items': cart_items,
-        'total': total,
+        'grand_total': grand_total,
         'product_count': product_count,
     }
 
