59b87314450dabac4820e2f133c6bf6c63811b64
Simon Castagna <sd.castagna@gmail.com>
2020-10-31 14:18:56 +0100

    added signals to update order total on save or deletion of line item

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date', 'order_total')
+
+    fields = ('order_number', 'date', 'company_name', 'email',
+              'phone_number', 'delivery_address', 'postcode',
+              'city', 'order_total',)
+
+    list_display = ('order_number', 'date', 'company_name', 'order_total')
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
