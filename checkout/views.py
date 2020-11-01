22233ff2d75b10b1484a2a0bfea036fa2f0e036c
Simon Castagna <sd.castagna@gmail.com>
2020-11-01 13:39:07 +0100

    added checkout success logic and template

--- checkout/views.py
+++ checkout/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
-from .models import OrderLineItem
+from .models import Order, OrderLineItem
 from cart.contexts import cart_contents
 from products.models import Product
 
@@ -24,7 +24,6 @@ def checkout(request):
             'postcode': request.POST['postcode'],
             'city': request.POST['city'],
         }
-
         order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save()
@@ -77,3 +76,46 @@ def checkout(request):
     }
 
     return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save-info')
+    order = get_object_or_404(Order, order_number=order_number)
+
+    # if request.user.is_authenticated:
+    #     profile = UserProfile.objects.get(user=request.user)
+    #     # Attach the user's profile to the order
+    #     order.user_profile = profile
+    #     order.save()
+    #     # Save the user's info
+    #     if save_info:
+    #         profile_data = {
+    #             'default_phone_number': order.phone_number,
+    #             'default_country': order.country,
+    #             'default_postcode': order.postcode,
+    #             'default_town_or_city': order.town_or_city,
+    #             'default_street_address1': order.street_address1,
+    #             'default_street_address2': order.street_address2,
+    #             'default_county': order.county,
+    #         }
+    #         user_profile_form = UserProfileForm(profile_data, instance=profile)
+    #         if user_profile_form.is_valid():
+    #             user_profile_form.save()
+
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'cart' in request.session:
+        del request.session['cart']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
+
+    return render(request, template, context)
+
