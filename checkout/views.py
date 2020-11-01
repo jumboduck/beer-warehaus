a68d57dbd4ceb9db75c7ff7e602cd88e3be0b2e8
Simon Castagna <sd.castagna@gmail.com>
2020-11-01 13:00:37 +0100

    orders are saved in db when payment is submitted

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from .models import OrderLineItem
 from cart.contexts import cart_contents
+from products.models import Product
 
 import stripe
 
@@ -12,21 +14,57 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    cart = request.session.get('cart', {})
-    if not cart:
-        messages.error(request, 'Your cart is currently empty.')
-        return redirect(reverse('products'))
+    if request.method == 'POST':
+        cart = request.session.get('cart', {})
+        form_data = {
+            'company_name': request.POST['company_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'delivery_address': request.POST['delivery_address'],
+            'postcode': request.POST['postcode'],
+            'city': request.POST['city'],
+        }
 
-    current_cart = cart_contents(request)
-    total = current_cart['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, quantity in cart.items():
+                try:
+                    product = get_object_or_404(Product, pk=item_id)
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        product=product,
+                        quantity=quantity,
+                    )
+                    order_line_item.save()
 
-    order_form = OrderForm()
+                except Product.DoesNotExist:
+                    messages.error(request, 'One of the products in your cart was not found.')
+                    order.delete()
+                    return redirect(reverse('view_cart'))
+
+            request.session['save-info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+
+        else:
+            messages.error(request, 'There was an error with the information you entered. Please try again.')
+
+    else:
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(request, 'Your cart is currently empty.')
+            return redirect(reverse('products'))
+
+        current_cart = cart_contents(request)
+        total = current_cart['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. Ensure it is set in the environment.')
