7d398ed29822aaa7dad9840bc67f6ad33b5d7353
Simon Castagna <sd.castagna@gmail.com>
2020-11-13 12:32:35 +0100

    created profiles app and UserProfile model

--- beer_warehaus/settings.py
+++ beer_warehaus/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'producers',
     'cart',
     'checkout',
+    'profiles',
 
     # Other
 
---
+++ profiles/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class UserProfile(models.Model):
+    """
+    User Profile model which holds order history and saved delivery information
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_company_name = models.CharField(max_length=50, null=True, blank=True)
+    default_email = models.EmailField(max_length=254, null=True, blank=True)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_delivery_address = models.CharField(max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_city = models.CharField(max_length=40, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """Create or update the user profile"""
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Existing users, just save the profile
+    instance.userprofile.save()
