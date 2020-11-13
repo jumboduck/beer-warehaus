d65a00cab6d79bb666cfbe68f92ce0867e62976c
Simon Castagna <sd.castagna@gmail.com>
2020-11-13 12:46:02 +0100

    created profile view and template

--- beer_warehaus/urls.py
+++ beer_warehaus/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('producers/', include('producers.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- checkout/models.py
+++ checkout/models.py
@@ -3,10 +3,13 @@ from django.db import models
 from django.db.models import Sum
 
 from products.models import Product
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     company_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
