65af206b2e5c2540b955cf2f64419cd42662a8f9
Simon Castagna <sd.castagna@gmail.com>
2020-11-21 14:19:34 +0100

    created edit slide view and template

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('manage_slides/', views.manage_slides, name='manage_slides'),
+    path('manage_slides/edit/<int:slide_id>/', views.edit_slide, name='edit_slide'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 from .models import Slide
+from .forms import SlideForm
 from products.models import Product
 
 
@@ -16,7 +20,13 @@ def index(request):
     return render(request, 'home/index.html', context)
 
 
+@login_required
 def manage_slides(request):
+    if not request.user.is_superuser:
+        # If user is not an admin, redirect to home page
+        messages.error(request, "Only store owners can access this page.")
+        return redirect(reverse('home'))
+
     slides = Slide.objects.all()
 
     context = {
@@ -24,3 +34,39 @@ def manage_slides(request):
     }
 
     return render(request, 'home/manage_slides.html', context)
+
+
+@login_required
+def edit_slide(request, slide_id):
+    """
+    # Edit an existing slide on the homepage
+    """
+    if not request.user.is_superuser:
+        # If user is not an admin, redirect to home page
+        messages.error(request, "Only store owners can access this page.")
+        return redirect(reverse('home'))
+
+    slide = get_object_or_404(Slide, pk=slide_id)
+
+    if request.method == 'POST':
+        form = SlideForm(request.POST, request.FILES, instance=slide)
+
+        # Ensure form is valid before updating the database
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated slide information.')
+            return redirect(reverse('manage_slides'))
+        else:
+            messages.error(request, 'There was an error updating this slide. \
+                Please ensure the form is valid.')
+
+    else:
+        form = SlideForm(instance=slide)
+        messages.info(request, f'You are editing the slide titled "{slide.title}".')
+    context = {
+            'form': form,
+            'slide': slide,
+        }
+    template = 'home/edit_slide.html'
+
+    return render(request, template, context)
