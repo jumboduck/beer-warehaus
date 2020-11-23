f65f12dc6d178f0c5f497b7287c96dfa6a99d286
Simon Castagna <sd.castagna@gmail.com>
2020-11-23 16:20:28 +0100

    updated local database to use postgresql

--- beer_warehaus/settings.py
+++ beer_warehaus/settings.py
@@ -150,8 +150,11 @@ WSGI_APPLICATION = 'beer_warehaus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'beerwarehaus',
+        'USER': os.environ.get('DATABASE_USER'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
