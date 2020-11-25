0948976f5c1b9a2fe7a3cea8ee8159fa96cd3867
Simon Castagna <sd.castagna@gmail.com>
2020-11-25 18:13:24 +0100

    updated contact template

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 def contact(request):
+
+    if request.POST:
+        to_email = settings.DEFAULT_ORDER_EMAIL
+        from_email = request.POST['email']
+
     template = 'contact/contact.html'
 
     return render(request, template)
