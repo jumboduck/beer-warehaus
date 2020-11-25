32aadc7b3697ae83df8d69e8ac7b48de6d8d4fef
Simon Castagna <sd.castagna@gmail.com>
2020-11-25 18:34:26 +0100

    updated contact view to send emails

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.contrib import messages
 from django.conf import settings
 
 
@@ -8,6 +10,35 @@ def contact(request):
     if request.POST:
         to_email = settings.DEFAULT_ORDER_EMAIL
         from_email = request.POST['email']
+        phone = request.POST['phone']
+        company = request.POST['company']
+        contact_person = request.POST['contact_person']
+        message = request.POST['message']
+
+        email_context = {
+            'company': company,
+            'phone': phone,
+            'contact_person': contact_person,
+            'message': message,
+        }
+
+        subject = render_to_string(
+            'contact/contact_emails/contact_email_subject.txt',
+            {'company': company}
+        )
+        body = render_to_string(
+            'contact/contact_emails/contact_email_body.txt', email_context
+        )
+
+        send_mail(
+            subject,
+            body,
+            from_email,
+            [to_email],
+        )
+
+        messages.success(request, "Your message was sent successfully. You will hear back from us soon.")
+        return redirect(reverse('home'))
 
     template = 'contact/contact.html'
 
