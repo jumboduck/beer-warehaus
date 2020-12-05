5700e96bcca192f61bc3de9118258f4fc434bd67
Simon Castagna <sd.castagna@gmail.com>
2020-12-05 21:01:44 +0100

    fixed settings issue with upload of images to S3

--- beer_warehaus/settings.py
+++ beer_warehaus/settings.py
@@ -224,7 +224,7 @@ if 'USE_AWS' in os.environ:
     # Static and Media Files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
-    MEDIAFILES_STORAGE = 'custom_storages.MediaStorage'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
