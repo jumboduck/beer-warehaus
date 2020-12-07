2c82c596b3950a468ddc175bf5128d80492c7cb8
Simon Castagna <sd.castagna@gmail.com>
2020-12-07 12:00:53 +0100

    updated save function for products and producers to not upload image to aws and use image from API instead

--- producers/models.py
+++ producers/models.py
@@ -15,19 +15,3 @@ class Producer(models.Model):
 
     def __str__(self):
         return self.name
-
-    """
-    Update to save method so that any image in the 'Image url' field
-    will be saved on the server as the image field.
-    """
-    def save(self, *args, **kwargs):
-        if self.image_url and not self.image:
-            image_url = self.image_url
-            file_name = image_url.rsplit('/', 1)[-1]
-            response = requests.get(image_url)
-
-            img_temp = NamedTemporaryFile(delete=True)
-            img_temp.write(response.content)
-            img_temp.flush()
-            self.image.save(f'{self.name}_{file_name}', File(img_temp))
-        super(Producer, self).save(*args, **kwargs)
--- products/models.py
+++ products/models.py
@@ -69,22 +69,6 @@ class Product(models.Model):
         return self.name
 
     """
-    # Update to save method so that any image in the 'Image url' field
-    # will be saved on the server as the image field.
-    """
-    def save(self, *args, **kwargs):
-        if self.image_url and not self.image:
-            image_url = self.image_url
-            file_name = image_url.rsplit('/', 1)[-1]
-            response = requests.get(image_url)
-
-            img_temp = NamedTemporaryFile(delete=True)
-            img_temp.write(response.content)
-            img_temp.flush()
-            self.image.save(f'{self.name}_{file_name}', File(img_temp))
-        super(Product, self).save(*args, **kwargs)
-
-    """
     # Calculate the cost of one crate of a product
     """
     @property
